74ebb4f27d0695038619d314a38ad736793af3d6
Clhahaha <1834731542@qq.com>
2021-02-27 19:26:30 +0800

    初始化项目

--- src/components/content/MainTabbar.vue
+++ src/components/content/MainTabbar.vue
@@ -2,23 +2,23 @@
   <div id="app">
     <Tabbar>
       <tabbar-item url="/home">
-        <img slot="item-icon" src="~assets/img/tabbarImg/home.png" alt="">
-        <img slot="item-icon-active" src="~assets/img/tabbarImg/home1.png" alt="">
+        <img slot="item-icon" src="~assets/img/tabbarImg/icon01.png" alt="">
+        <img slot="item-icon-active" src="~assets/img/tabbarImg/icon01_active.png" alt="">
         <div slot="item-text">首页</div>
       </tabbar-item>
       <tabbar-item url="/fenlei">
-        <img slot="item-icon" src="~assets/img/tabbarImg/fenlei.png" alt="">
-        <img slot="item-icon-active" src="~assets/img/tabbarImg/fenlei1.png" alt="">
+        <img slot="item-icon" src="~assets/img/tabbarImg/icon02.png" alt="">
+        <img slot="item-icon-active" src="~assets/img/tabbarImg/icon02_active.png" alt="">
         <div slot="item-text">分类</div>
       </tabbar-item>
       <tabbar-item url="gouwuche">
-        <img slot="item-icon" src="~assets/img/tabbarImg/gouwuche.png" alt="">
-        <img slot="item-icon-active" src="~assets/img/tabbarImg/gouwuche1.png" alt="">
+        <img slot="item-icon" src="~assets/img/tabbarImg/icon03.png" alt="">
+        <img slot="item-icon-active" src="~assets/img/tabbarImg/icon03_active.png" alt="">
         <div slot="item-text">购物车</div>
       </tabbar-item>
       <tabbar-item url="wod">
-        <img slot="item-icon" src="~assets/img/tabbarImg/wod.png" alt="">
-        <img slot="item-icon-active" src="~assets/img/tabbarImg/wod1.png" alt="">
+        <img slot="item-icon" src="~assets/img/tabbarImg/icon04.png" alt="">
+        <img slot="item-icon-active" src="~assets/img/tabbarImg/icon04_active.png" alt="">
         <div slot="item-text">我的</div>
       </tabbar-item>
     </Tabbar>
