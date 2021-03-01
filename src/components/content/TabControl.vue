c51e94f15705065fa9ec25dd554a255aaacbbb8a
Clhahaha <1834731542@qq.com>
2021-03-01 09:04:04 +0800

    haha

---
+++ src/components/content/TabControl.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="tabcontrol">
+      <div class="tabcontrolItem" v-for="(item,index) in titles" :key="index" :class="{active:index===currentIndex}" @click="itemClick(index)">
+          <span>{{item}}</span>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    props:{
+        titles:{
+            type:Array,
+            default(){
+                return[]
+            }
+        }
+    },
+    data(){
+        return{
+            currentIndex:0
+        }
+    },
+    methods:{
+        itemClick(index){
+            this.currentIndex=index
+        }
+    }
+}
+</script>
+
+<style>
+    .tabcontrol{
+        width: 100%;
+        background-color: rgb(255, 255, 255);
+        display: flex;
+        text-align: center;
+        font-size: 15px;
+        padding-bottom: 5px;
+    }
+    .tabcontrolItem{
+        flex: 1;
+        height: 40px;
+        line-height: 40px;
+    }
+    .tabcontrolItem span{
+        padding: 5px;
+    }
+    .active{
+        color: #ff8198;
+    }
+    .active span{
+        border-bottom: 3px solid #ff5777;
+    }
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,13 +3,82 @@
     <nav-bar class="home-nav"><h3 slot="center">购物街</h3></nav-bar>
     <home-swiper :banners="banners"></home-swiper>
     <home-recommend-view :recommends="recommends"></home-recommend-view>
+    <home-featrue></home-featrue>
+    <tab-control class="tabcontrol" :titles="['流行','新款','精选']"></tab-control>
+    <div>
+      <ul>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+        <li>列表</li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-import NavBar from 'components/common/navbar/NavBar.vue'//导入导航栏
 import HomeSwiper from "./childComps/HomeSwiper"//导入轮播图
 import HomeRecommendView from "./childComps/HomeRecommendView"//导入推荐视图
+import HomeFeatrue from "./childComps/HomeFeature"//导入流行图
+
+import NavBar from 'components/common/navbar/NavBar.vue'//导入导航栏
+import TabControl from "components/content/TabControl"//导入功能栏
 
 import {getHomeMultidata} from "network/home" //导入网络模块
 
@@ -17,13 +86,15 @@ import {getHomeMultidata} from "network/home" //导入网络模块
 export default {
     name:'home',
     components:{
-      NavBar,
       HomeSwiper,
-      HomeRecommendView
+      HomeRecommendView,
+      HomeFeatrue,
+
+      NavBar,
+      TabControl
     },
     data(){
       return{
-        // result:null
         banners:[],
         recommends:[]
       }
@@ -41,8 +112,21 @@ export default {
 </script>
 
 <style>
+  #home{
+    padding-top: 44px;/* 因为固定定位脱离了标准流，为了让导航栏正常显示，预留顶部44px的位置 */
+  }
   .home-nav{
+    position: fixed;/* 固定定位 */
+    left: 0;
+    right: 0;
+    top: 0;
+     z-index: 1;/*提高权重 */
     background-color: #ff5777;
     color: white;
+  }
+
+  .tabcontrol{
+     position: sticky;/*粘性固定  用于功能栏 */
+     top: 44px;
   }
 </style>
